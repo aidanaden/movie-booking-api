cd160835a73c45c3cfeadf053dda7cdce0ea3ff0
aidan-wsl <aidan@DESKTOP-73NEEJC.localdomain>
2021-12-17 18:00:02 +0800

    remove driver return

--- heehaw.py
+++ heehaw.py
@@ -355,8 +355,7 @@ def scrapeReviewsForMovie(movieName, driver):
     driver.switch_to.window(driver.window_handles[0])
 
     print('driver object: {driver}')
-
-    return (reviewDatas, driver)
+    return reviewDatas
 
 CHROMEDRIVER_PATH = '/home/aidan/chromedriver'
 WINDOW_SIZE = "1920,1080"
@@ -387,7 +386,7 @@ movies = scrapeCathay(driver, movies, tmdbUrl, tmdbSearchUrl, params)
 
 print('creating movie object data...')
 for movie in movies:
-    (reviews, driver) = scrapeReviewsForMovie(movie['movie'], driver)
+    reviews = scrapeReviewsForMovie(movie['movie'], driver)
     movie['reviews'] = reviews
     Movie.objects.create(data=movie)
 
