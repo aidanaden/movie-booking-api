3894eb6ff87253084e2a9f1c1509e6cf0f9c30c0
aidan-wsl <aidan@DESKTOP-73NEEJC.localdomain>
2021-12-17 21:21:48 +0800

    remove GV scrape, add SHAW scrape, test driver session invalid id

--- heehaw.py
+++ heehaw.py
@@ -1,5 +1,5 @@
 import enum
-import re, requests, datetime
+import re, requests, datetime, time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
@@ -271,6 +271,100 @@ def scrapeCathay(driver, movies, tmdbUrl, tmdbSearchUrl, params):
     
     return movies
 
+# SCRAPE SHAW
+def scrapeShaw(driver, movies, tmdbUrl, tmdbSearchUrl, params):
+    driver.get("https://www.shaw.sg/movie")
+
+    movieFields = driver.find_elements(By.XPATH, "//div[contains(@class, 'info')]")
+    movieUrls = [movieField.find_element(By.TAG_NAME, 'a').get_attribute('href') for movieField in movieFields]
+    movieNames = [movieField.find_element(By.TAG_NAME, 'a').text for movieField in movieFields]
+
+    for i, (movieUrl, movieName) in enumerate(zip(movieUrls, movieNames)):
+        cleanedMovieName = cleanTitle(movieName)
+        print(f'movie name: {movieName}\ncleaned movie name: {cleanedMovieName}\nmovie url: {movieUrl}\n')
+
+        params['query'] = cleanedMovieName
+        searchResultInfo = requests.get(tmdbSearchUrl, params=params).json()
+
+        if len(searchResultInfo['results']) > 0:
+            movieId = searchResultInfo['results'][0]['id']
+            movieInfo = getMovieFromId(movieId, tmdbUrl, params['api_key'])
+
+            movieData = {
+                'movie': movieName,
+                'info': movieInfo,
+                'cinemas': []
+            }
+
+            driver.get(movieUrl)
+            cinemaDatesFields = []
+            
+            try:
+                cinemaDatesFields = driver.find_element(By.CLASS_NAME, 'owl-stage').find_elements(By.XPATH, './div')
+            except:
+                print('no movie timings available, skipping...')
+                continue
+
+            rightClickBtnField = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[2]/div[8]/div/div/div/div[2]/div/div[1]/div/div[2]/div[2]/div')
+
+            for j in range(len(cinemaDatesFields)):
+                if j >= 3:
+                    print('clicking on right click button...')
+                    rightClickBtnField.click()
+                    time.sleep(3)
+                
+                time.sleep(1)
+                cinemaDateField = driver.find_element(By.CLASS_NAME, 'owl-stage').find_element(By.XPATH, f'./div[{j+1}]')
+                cinemaDateFields = cinemaDateField.find_elements(By.TAG_NAME, 'span')
+
+                if (len(cinemaDateFields) <= 0):
+                    continue
+                
+                print(f'length of cinema dates: {len(cinemaDateFields)}')
+                cinemaDate = cinemaDateFields[-1].text
+                print(f'cinema date selected: {cinemaDate}')
+                # click on date and wait for timings to load
+                cinemaDateField.click()
+                time.sleep(3)
+
+                cinemaFields = driver.find_element(By.XPATH, "//div[contains(@id, 'moviesDiv')]").find_elements(By.XPATH, './div')
+                for k, cinemaField in enumerate(cinemaFields):
+                    if k == 0:
+                        continue
+                    else:
+                        cinemaName = cinemaField.find_element(By.XPATH, './div[1]').text
+                        print(f'=== {cinemaName} ===')
+
+                        cinemaData = {
+                            'cinema': cinemaName,
+                            'timings': []
+                        }
+
+                        cinemaTimingFields = cinemaField.find_element(By.XPATH, './div[2]').find_elements(By.XPATH, './div')
+                        for cinemaTimingField in cinemaTimingFields:
+                            cinemaTiming = cinemaTimingField.text
+                            cinemaTimingUrl = cinemaTimingField.find_element(By.TAG_NAME, 'a').get_attribute('href')
+                            timingData = {
+                                'timing': f'{cinemaDate} {cinemaTiming}',
+                                'url': cinemaTimingUrl
+                            }
+                            print(timingData)
+                            cinemaData['timings'].append(timingData)
+                    
+                        if len(movieData['cinemas']) > k-1:
+                            movieData['cinemas'][k-1]['timings'] += cinemaData['timings']
+                        else:
+                            movieData['cinemas'].append(cinemaData)
+
+            movieExists = False
+            for movie in movies:
+                if movie['info']['id'] == movieData['info']['id']:
+                    movieExists = True
+                    movie['cinemas'] += movieData['cinemas']
+            if movieExists == False:
+                movies.append(movieData)
+
+        return movies
 
 def scrapeReviewsForMovie(movieName, driver):
     print(f'finding reviews for movie: {movieName}')
@@ -279,11 +373,11 @@ def scrapeReviewsForMovie(movieName, driver):
     searchQueryUrl = f"https://www.rottentomatoes.com/search?search={queryMovieName}"
     print(f'query movie name: {queryMovieName}')
     print(f'search query url: {searchQueryUrl}')
-    driver.execute_script("window.open()")
+    # driver.execute_script("window.open()")
     # Switch to the newly opened tab
-    driver.switch_to.window(driver.window_handles[1])
+    # driver.switch_to.window(driver.window_handles[1])
     driver.get(searchQueryUrl)
-    driver.implicitly_wait(1)
+    time.sleep(1)
 
     movieUrl = ''
     try:
@@ -309,7 +403,7 @@ def scrapeReviewsForMovie(movieName, driver):
 
         try:
             driver.get(movieReviewsUrl)
-            driver.implicitly_wait(2)
+            time.sleep(2)
         except WebDriverException:
             error = """
             Message: unknown error: session deleted because of page crash
@@ -370,8 +464,8 @@ def scrapeReviewsForMovie(movieName, driver):
                 print('review rating does not exist, skipping...')
                 continue
 
-        driver.close()
-        driver.switch_to.window(driver.window_handles[0])
+        # driver.close()
+        # driver.switch_to.window(driver.window_handles[0])
 
         print(f'driver object: {driver}')
         return reviewDatas
@@ -400,8 +494,9 @@ params = {
 movies = []
 Movie.objects.all().delete()
 
-movies = scrapeGV(driver, movies, tmdbUrl, tmdbSearchUrl, params)
+# movies = scrapeGV(driver, movies, tmdbUrl, tmdbSearchUrl, params)
 movies = scrapeCathay(driver, movies, tmdbUrl, tmdbSearchUrl, params)
+movies = scrapeShaw(driver, movies, tmdbUrl, tmdbSearchUrl, params)
 
 print('creating movie object data...')
 for movie in movies:
