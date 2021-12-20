98421cb452e022c03bfb5be39d051c24f4f701d3
aidan-wsl <aidan@DESKTOP-73NEEJC.localdomain>
2021-12-20 19:08:13 +0800

    add class based detail view 3

--- api/views.py
+++ api/views.py
@@ -33,3 +33,4 @@ class MovieListAPIView(ListAPIView):
 class MovieDetailAPIView(RetrieveAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+    lookup_field = 'id'
