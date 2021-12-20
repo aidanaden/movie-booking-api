40f5c7ac515a21d4360dcb0147e1085812bbc8c8
aidan-wsl <aidan@DESKTOP-73NEEJC.localdomain>
2021-12-20 19:19:56 +0800

    disable debug in production

--- api/views.py
+++ api/views.py
@@ -8,24 +8,6 @@ from .serializers import MovieSerializer
 from rest_framework.generics import ListAPIView
 from rest_framework.generics import RetrieveAPIView
 
-# Create your views here.
-# @csrf_exempt
-# def movieApi(request):
-#     if request.method == 'GET':
-#         movies = Movie.objects.all()
-#         movies_serializer = MovieSerializer(movies, many=True)
-#         return JsonResponse(movies_serializer.data, safe=False)
-
-# @csrf_exempt
-# def movieDetailApi(request):
-#     if request.method == 'GET':
-#         id = request.query_params['id']
-#         movie = Movie.objects.all(id=id)
-#         movie_serializer = MovieSerializer(movie, many=False)
-#         return JsonResponse(movie_serializer.data, safe=False)
-        
-        
-
 class MovieListAPIView(ListAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
@@ -33,4 +15,4 @@ class MovieListAPIView(ListAPIView):
 class MovieDetailAPIView(RetrieveAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
-    lookup_field = 'id'
+    lookup_field = 'slug'
