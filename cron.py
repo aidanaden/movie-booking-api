d17a8da689cffac92e2da8230913996040321905
aidan-wsl <aidan@DESKTOP-73NEEJC.localdomain>
2021-12-24 04:29:41 +0800

    add cron python script 2

--- cron.py
+++ cron.py
@@ -1,7 +1,7 @@
 from crontab import CronTab
 
 with CronTab(user='aidan') as cron:
-    job = cron.new(command='source /home/aidan/movie-booking-api/venv/bin/activate; $(which python3) /home/aidan/movie-booking-api/manage.py runscript /home/aidan/movie-booking-api/heehaw.py')
+    job = cron.new(command='source ~/movie-booking-api/venv/bin/activate; $(which python3) ~/movie-booking-api/manage.py runscript heehaw.py')
     job.every(3).hours()
 
 print('cron.write() was just executed')
