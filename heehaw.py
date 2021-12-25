4407cfa9f95aee2c39c4fd72116ca58ed8eee766
aidan-wsl <aidan@DESKTOP-73NEEJC.localdomain>
2021-12-25 22:22:31 +0800

    add gv back

--- heehaw.py
+++ heehaw.py
@@ -620,7 +620,7 @@ params = {
 
 movies = []
 
-# movies = scrapeGV(driver, movies, tmdbUrl, tmdbSearchUrl, params)
+movies = scrapeGV(driver, movies, tmdbUrl, tmdbSearchUrl, params)
 movies = scrapeCathay(driver, movies, tmdbUrl, tmdbSearchUrl, params)
 movies = scrapeShaw(driver, movies, tmdbUrl, tmdbSearchUrl, params)
 
