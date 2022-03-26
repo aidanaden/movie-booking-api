e4e65bd6a0e57f6d5a86833d133f2f9e8f4e35d1
aidanaden <aidanaden@hotmail.com>
2022-03-26 19:03:23 +0800

    Run tmdb query after scrape 8

--- scrape.py
+++ scrape.py
@@ -283,7 +283,7 @@ def scrapeCathay(driver, movies, tmdbUrl, tmdbSearchUrl, params):
     time.sleep(3)
 
     moviesContainerClass = 'boxes'
-    movieContainerFields = WebDriverWait(driver, 10).find_elements(By.CLASS_NAME, moviesContainerClass)
+    movieContainerFields = driver.find_elements(By.CLASS_NAME, moviesContainerClass)
 
     movieUrlFields = [movieContainerField.find_element(By.TAG_NAME, 'a') for movieContainerField in movieContainerFields]
     movieUrls = set([movieField.get_attribute('href') for movieField in movieUrlFields])
